a805244809850470a58066399e539fb575426f8e
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-04-26 20:12:19 -0500

    Change in data input

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import Header from '../components/header';
 import CardList from '../components/CardList';
 import OverviewCardList from '../components/overviewCardList';
-import Data from '../assets/data.json'
 import '../assets/styles/App.css';
 
-function App() {
+function App(props) {
+  const {
+    info,
+    data,
+    overview
+  } = props;
+
   const [light, setLight] = useState(true);
 
   const body = document.querySelector('body');
@@ -14,9 +19,9 @@ function App() {
 
   return (
     <div className={`app container-lg mt-5 mx-4 mx-lg-auto p-0`}>
-      <Header totalFollowers={Data.info.totalFollowers} light={light} setLight={setLight}/>
-      <CardList cards={Data.data} light={light}/>
-      <OverviewCardList overviewCards={Data.overview} light={light}/>
+      <Header totalFollowers={info.totalFollowers} light={light} setLight={setLight}/>
+      <CardList cards={data} light={light}/>
+      <OverviewCardList overviewCards={overview} light={light}/>
     </div>
   );
 }
