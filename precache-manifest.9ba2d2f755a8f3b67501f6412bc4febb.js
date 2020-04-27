1f5441731614ab89a6e051eaac427cbf1e8760bd
Esteban Ladino <esteban.ladino.927@gmail.com>
2020-04-26 19:57:33 -0500

    Updates

---
+++ precache-manifest.9ba2d2f755a8f3b67501f6412bc4febb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcd52bd9cbf48cfc18e4c11b76da58d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57ac4c2272aa916c3806",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "6569d4e91fa4f4e46855",
+    "url": "/static/css/main.5feb62aa.chunk.css"
+  },
+  {
+    "revision": "57ac4c2272aa916c3806",
+    "url": "/static/js/2.2c200ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c200ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6569d4e91fa4f4e46855",
+    "url": "/static/js/main.9383a69f.chunk.js"
+  },
+  {
+    "revision": "e75d660a1997be3cba22",
+    "url": "/static/js/runtime-main.c407ebb3.js"
+  },
+  {
+    "revision": "0930073eae9c4dbebce3d43dc419f7b5",
+    "url": "/static/media/Inter.0930073e.ttf"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
